1b6cede7cf2d8a189ead6fa5ac0f6d148a838610
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-08-31 12:08:52 +0700

    update

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function toggleMenu() {
+  let menuToggle = document.querySelector('.toggle');
+  let menu = document.querySelector('.menu');
+  menuToggle.classList.toggle('active');
+  menu.classList.toggle('active');
+}
