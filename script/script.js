93742a8c41b9749c102e5d1319a293764cbd1ffc
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-11-25 12:19:07 +0700

    .

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-window.addEventListener("scroll",function(){document.querySelector("header").classList.toggle("sticky",window.scrollY>0)}),document.querySelector(".toggle").addEventListener("click",()=>{let e=document.querySelector(".toggle"),t=document.querySelector(".menu");e.classList.toggle("active"),t.classList.toggle("active")});
+window.addEventListener("scroll", function () {
+  document
+    .querySelector("header")
+    .classList.toggle("sticky", window.scrollY > 0);
+}),
+  document.querySelector(".toggle").addEventListener("click", () => {
+    let e = document.querySelector(".toggle"),
+      t = document.querySelector(".menu");
+    e.classList.toggle("active"), t.classList.toggle("active");
+  });
